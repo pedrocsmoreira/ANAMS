7f94a7a81300d8d90d0763efc3f2089664e2bde7
pedrocsmoreira <81022217+pedrocsmoreira@users.noreply.github.com>
2022-09-27 09:00:31 +0100

    atributes, getters and setters class clinica

--- it1/src/main/java/com/anams/Model/Clinica.java
+++ it1/src/main/java/com/anams/Model/Clinica.java
@@ -1,5 +1,95 @@
 package com.anams.Model;
 
+import java.util.ArrayList;
+
 public class Clinica {
-    
+    private String nome;
+    private String endereco;
+    private int nif;
+    private int contacto;
+    private String website;
+
+    private ArrayList<Especialidade> listaEspecialidades;
+    private ArrayList<Medico> listaMedicos;
+    private ArrayList<TipoServico> listaTipoServicos;
+    private ArrayList<Servico> listaServicos;
+    private ArrayList<Convencao> listaConvencoes;
+
+    private static final String STR_DEFAULT = "";
+    private static final int INT_DEFAULT = 0;
+
+    private Clinica(){
+        this.nome = STR_DEFAULT;
+        this.endereco = STR_DEFAULT;
+        this.nif = INT_DEFAULT;
+        this.contacto = INT_DEFAULT;
+        this.website = STR_DEFAULT;
+        this.listaEspecialidades = new ArrayList<Especialidade>();
+        this.listaMedicos = new ArrayList<Medico>();
+        this.listaTipoServicos = new ArrayList<TipoServico>();
+        this.listaServicos = new ArrayList<Servico>();
+        this.listaConvencoes =  new ArrayList<Convencao>();
+    }
+
+    public void setNome(String nome){ this.nome = nome; }
+
+    public String getNome(){ return this.nome; }
+
+    public void setEndereco(String endereco){ this.endereco = endereco; }
+
+    public String getEndereco(){ return this.endereco; }
+
+    public void setNif(int nif){ this.nif = nif; }
+
+    public int getNif(){ return this.nif; }
+
+    public void setContacto(int contacto){ this.contacto = contacto; }
+
+    public int getContacto(){ return this.contacto; }
+
+    public void setWebsite(String website){ this.website = website; }
+
+    public String getWebsite(){ return this.website; }
+
+
+    public void setListaEspecialidades(ArrayList<Especialidade> listaEspecialidades){
+        this.listaEspecialidades = listaEspecialidades;
+    }
+
+    public ArrayList<Especialidade> getListaEspecialidades(){
+        return this.listaEspecialidades;
+    }
+
+    public void setListaMedicos(ArrayList<Medico> listaMedicos){
+        this.listaMedicos = listaMedicos;
+    }
+
+    public ArrayList<Medico> getListaMedicos(){
+        return this.listaMedicos;
+    }
+
+    public void setListaTipoServicos(ArrayList<TipoServico> listaTipoServicos){
+        this.listaTipoServicos = listaTipoServicos;
+    }
+
+    public ArrayList<TipoServico> getListaTipoServicos(){
+        return this.listaTipoServicos;
+    }
+
+    public void setListaServicos(ArrayList<Servico> listaServicos){
+        this.listaServicos = listaServicos;
+    }
+
+    public ArrayList<Servico> getListaServicos(){
+        return this.listaServicos;
+    }
+
+    public void setListaConvencoes(ArrayList<Convencao> listaConvencoes){
+        this.listaConvencoes = listaConvencoes;
+    }
+
+    public ArrayList<Convencao> getListaConvencoes(){
+        return this.listaConvencoes;
+    }
+
 }
