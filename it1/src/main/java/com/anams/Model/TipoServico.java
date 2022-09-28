0743777c3313cfbf3f571eeb00a7d2286b954c03
pedrocsmoreira <81022217+pedrocsmoreira@users.noreply.github.com>
2022-09-28 10:42:56 +0100

    tipo servico entire class

--- it1/src/main/java/com/anams/Model/TipoServico.java
+++ it1/src/main/java/com/anams/Model/TipoServico.java
@@ -1,10 +1,59 @@
 package com.anams.Model;
 
 public class TipoServico {
-    private int codigoUnico;
+    private int codigo;
     private String nome;
 
     private static int contador = 0;
 
     private static final String STR_DEFAULT = "";
+
+    /**
+     * 
+     */
+    public TipoServico(){
+        this.codigo = ++contador;
+        this.nome = STR_DEFAULT;
+    }
+
+    /**
+     * @param nome
+     */
+    public TipoServico(String nome){
+        this.codigo = ++contador;
+        this.nome = nome;
+    }
+
+    /**
+     * @return codigo
+     */
+    public int getCodigo(){ return this.codigo; }
+
+    /**
+     * @param nome
+     */
+    public void setNome(String nome){ this.nome = nome; }
+
+    /**
+     * @return
+     */
+    public String getNome(){ return this.nome; }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n\tCódigo -> ");
+        sb.append(this.codigo);
+        sb.append("\n\tNome -> ");
+        sb.append(this.nome);
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object outro){
+        if(this == outro){ return true; }
+        if(outro == null || this.getClass() != outro.getClass()){ return false; }
+        TipoServico obj = (TipoServico) outro;
+        return this.codigo == obj.codigo && this.nome.equals(obj.nome);
+    }
+
 }
