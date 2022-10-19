5a81128222a163b0a6e192efc9e8abdada5a8e53
pedrocsmoreira <81022217+pedrocsmoreira@users.noreply.github.com>
2022-10-19 15:31:30 +0100

    update

--- it1/src/main/java/com/anams/Controller/UC4Controller.java
+++ it1/src/main/java/com/anams/Controller/UC4Controller.java
@@ -17,18 +17,44 @@ public class UC4Controller {
         this.clinica = c;
     }
 
+    /**
+     * Criar novo serviço, sem dados
+     * 
+     */
     public void novoServico(){ this.s = new Servico(); }
     
+    /**
+     * Definir o código 
+     * 
+     * @param codigo
+     */
     public void setCodigo(int codigo){ s.setCodigo(codigo); }
 
+    /**
+     * @param designacao
+     */
     public void setDesignacao(String designacao){ s.setDesignacao(designacao); }
 
+    /**
+     * @param preco
+     */
     public void setPreco(float preco){ s.setPreco(preco); }
 
+    /**
+     * @param ts
+     */
     public void setTipoServico(TipoServico ts){ s.setTipoServico(ts); }
 
+    /**
+     * 
+     * 
+     * @return s
+     */
     public Servico getServico() { return this.s; }
 
+    /**
+     * @throws ExceptionServicoExiste
+     */
     public void registarServico() throws ExceptionServicoExiste { clinica.registarServico(s); }
 
 }
--- it1/src/main/java/com/anams/Controller/UC5Controller.java
+++ it1/src/main/java/com/anams/Controller/UC5Controller.java
@@ -13,19 +13,58 @@ public class UC5Controller {
         this.clinica = c;
     }
 
+    /**
+     * Criar nova convenção, sem dados
+     * 
+     */
     public void novaConvencao(){ this.c = new Convencao(); }
 
+    /**
+     * Definir o código de uma convenção
+     * 
+     * @param codigo
+     */
     public void setCodigo(int codigo) { this.c.setCodigo(codigo); }
 
+    /**
+     * Definir a sigla de uma convenção
+     * 
+     * @param sigla
+     */
     public void setSigla(String sigla) { this.c.setSigla(sigla); }
 
+    /**
+     * Definir o nome de uma convenção
+     * 
+     * @param nome
+     */
     public void setNome(String nome) { this.c.setNome(nome); }
 
+    /**
+     * Definir a data de uma convenção
+     * 
+     * @param data
+     */
     public void setDataConvencao(Data data) { this.c.setDataConvencao(data); }
 
+    /**
+     * Definir o website de uma convenção
+     * 
+     * @param website
+     */
     public void setWebsite(String website) { this.c.setWebsite(website); }
 
+    /**
+     * Devolver uma convenção
+     * 
+     * @return c
+     */
     public Convencao getConvencao() { return this.c; }
 
+    /**
+     * Registar convenção na clínica
+     * 
+     * @throws ExceptionConvencaoExiste
+     */
     public void registarConvencao() throws ExceptionConvencaoExiste { clinica.registarConvencao(c); }
 }
