b2c71f61d6966d5c114c80768a3239742748212c
pedrocsmoreira <81022217+pedrocsmoreira@users.noreply.github.com>
2022-11-18 13:31:02 +0000

    marcacao

--- it1/src/main/java/com/anams/Model/Marcacao.java
+++ it1/src/main/java/com/anams/Model/Marcacao.java
@@ -8,4 +8,39 @@ public class Marcacao {
     private Cliente cl;
     private Data dia;
 
+    private static final int INT_DEFAULT = 0;
+    private static final String STR_DEFAULT = "";
+
+    public Marcacao(){
+        setId(INT_DEFAULT);
+        setMedico(new Medico());
+        setCliente(new Cliente());
+        setDia(new Data());
+    }
+
+    public Marcacao(int id, Medico med, Cliente cl, Data dia){
+        setId(id);
+        setMedico(med);
+        setCliente(cl);
+        setDia(dia);
+    }
+
+    public void setId(int id) { this.id = id; }
+
+    public int getId() { return this.id; }
+
+    public void setMedico(Medico med) { this.med = med; }
+
+    public Medico getMedico() { return this.med; }
+    
+    public void setCliente(Cliente cl) { this.cl = cl; }
+
+    public Cliente getCliente() { return this.cl; }
+
+    public void setDia(Data dia) { this.dia = dia; }
+
+    public Data getDia() { return this.dia; }
+
+
+
 }
